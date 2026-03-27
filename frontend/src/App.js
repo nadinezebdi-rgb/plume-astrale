212b78110a4be44eaea75ec31ff2d49e3d2c847a
nadinezebdi-rgb <nadine.zebdi@gmail.com>
2026-03-27 18:35:15 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,35 +15,15 @@ function App() {
     <AuthProvider>
       <BrowserRouter>
         <Navbar />
-
-// fallback simple
-const NotFound = () => (
-  <div style={{ color: "white", textAlign: "center", marginTop: "100px" }}>
-    Page non trouvée
-  </div>
-);
-
-function App() {
-  return (
-    <BrowserRouter>
-      <div style={{ minHeight: "100vh", background: "#0C0918" }}>
-
-        {/* Navbar */}
-        <Navbar />
-
-        {/* ROUTES */}
-        <Routes>
+<Routes>
           <Route path="/" element={<Index />} />
           <Route path="/inscription" element={<Register />} />
           <Route path="/connexion" element={<Login />} />
           <Route path="/tarot" element={<Tarot />} />
-
-          {/* fallback */}
-          <Route path="/" element={<div style={{color:"white"}}>SITE OK</div>} />
         </Routes>
 
-      </div>
-    </BrowserRouter>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
