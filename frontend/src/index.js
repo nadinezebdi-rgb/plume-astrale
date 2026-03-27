17b7a96d848f5f663931b7a1356709316dd30920
nadinezebdi-rgb <nadine.zebdi@gmail.com>
2026-03-27 14:12:09 +0100

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
-import "./index.css" ; import "./App.css" ;
-import App from "@/App";
+import "./index.css";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
     <HelmetProvider>
       <App />
     </HelmetProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
