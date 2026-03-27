548860647551e309d511ed2eef12ba7786278b65
nadinezebdi-rgb <nadine.zebdi@gmail.com>
2026-03-27 18:19:48 +0100

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,5 +1,5 @@
 import { useAuth } from '../context/AuthContext';
-import React, { useState } from 'react';
+
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '@/context/AuthContext';
 import { LogIn, Eye, EyeOff } from 'lucide-react';
