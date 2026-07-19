e0cfbaaebf1487926e252c54bc2590e2d6fa6b8e
nadinezebdi-rgb <nadine.zebdi@gmail.com>
2026-07-19 09:21:08 +0200

    Add ShootingStars component to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -68,7 +68,15 @@ import NoiseOverlay from "./components/design/NoiseOverlay";
 import Starfield from "./components/design/Starfield";
 import MobileTabBar from "./components/design/MobileTabBar";
 import AuraProvider from "./components/design/AuraProvider";
+import Starfield from "./components/design/Starfield";
+import ShootingStars from "./components/design/ShootingStars";   //
 
+{/* ─── Overlays globaux Plume Design System v2 ─── */}
+<Starfield />
+<ShootingStars />        
+<NoiseOverlay />
+<MobileTabBar />
+  
 function App() {
   return (
     <AuthProvider>
