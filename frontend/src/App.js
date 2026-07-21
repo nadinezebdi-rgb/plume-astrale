7d84df7c8c0c7fa4ae17c8cb5336dc3c6a44565f
nadinezebdi-rgb <nadine.zebdi@gmail.com>
2026-07-21 12:33:04 +0200

    Replace love-languages route with numerologie route
     Fix: ajout redirect /numerologie vers /outils/numerologie (issue #114).

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -184,7 +184,7 @@ function App() {
                 <Route path="/mon-rituel" element={<Navigate to="/outils/rituel" replace />} />
                 <Route path="/revolution-solaire" element={<Navigate to="/outils/revolution-solaire" replace />} />
                 <Route path="/horoscope" element={<Navigate to="/outils/horoscope" replace />} />
-                <Route path="/love-languages" element={<Navigate to="/outils/love-languages" replace />} />
+                <Route path="/numerologie" element={<Navigate to="/outils/numerologie" replace />} />
                 <Route path="/mot-de-passe-oublie" element={<MotDePasseOublie />} />
                 <Route path="/reinitialiser-mot-de-passe" element={<ReinitialiserMotDePasse />} />
                 <Route path="/notre-cadre" element={<NotreCadre />} />
